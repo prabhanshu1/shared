f03a12dbe24498733fd4415f8528e12ccb2eb6a9
Prabhanshu Abhishek <pabhi@iitk.ac.in>
2016-11-07 14:27:36 +0530

    Sending info about torrent from seed.html page

--- shared/static/static_dirs/js/seed.js
+++ shared/static/static_dirs/js/seed.js
@@ -1,49 +1,71 @@
-var client=new WebTorrent();
+var client = new WebTorrent();
 var torr;
-var fileStore=[];
+var fileStore = [];
+var torrentList =[];
 
-function addFiles(){
-    var fileList = document.getElementById("upload").files
-    fileStore.push.apply(fileStore,fileList)
-    showFilesxxx(fileStore,"selectedFiles");
+window.setInterval(function(){
+    $.ajax({
+        type: 'POST',
+        url: "updatedata",
+        data: {'torrent[]': torrentList },
+        async: true
+    });
+}, 5000);
+
+function addFiles() {
+    var fileList = document.getElementById("upload").files;
+    fileStore.push.apply(fileStore, fileList);
+    showFilesxxx(fileStore, "selectedFiles");
 }
 
-function stopSeeding(divId){
-    client.remove($(divId).attr("value"))
+function stopSeeding(divId) {
+    client.remove($(divId).attr("value"));
 }
 
 
-function startSeeding (divId) {
+function startSeeding(divId) {
 
-    client.seed(fileStore, function (torrent) {
+    client.seed(fileStore, function(torrent) {
         //fileStore=[];
+        torrentList.push(torrent.magnetURI);
+        torrentList.push(torrent.magnetURI);
+        torrentList.push(torrent.magnetURI);
 
-        $(divId).attr("value",torrent.magnetURI)
-        torr=torrent;
+        $(divId).attr("value", torrent.magnetURI);
+        torr = torrent;
 
-        setInterval(function(){updateStats(torrent,"showprogress");}, 1000)
+        setInterval(function() {
+            updateStats(torrent, "showprogress");
+        }, 1000);
 
-        torrent.files.forEach(function (file) {
-            file.appendTo('.log',{autoplay:false}, function (err, elem) {
-                if (err) throw err // file failed to download or display in the DOM
-            })
-        })
+        torrent.files.forEach(function(file) {
+            file.appendTo('.log', {
+                autoplay: false
+            }, function(err, elem) {
+                if (err) throw err; // file failed to download or display in the DOM
+            });
+        });
         $.ajax({
             type: 'POST',
-            url:"postdata",
-            data:{name:"Chutiya",magnetURI: torrent.magnetURI},
-            async:true
-        })
+            url: "postdata",
+            data: {
+                name: "Chutiya",
+                magnetURI: torrent.magnetURI
+            },
+            async: true
+        });
+
         
+
         // $.post("postdata",{name:"Chutiya",magnetURI: torrent.magnetURI})
         //     .done(function(data){
         //         $("#returnedData").html(data)
         //     })
-        console.log(client.torrents)
-    })
+        console.log(client.torrents);
+    });
 
     //    onFiles(jQuery(this).val())
-    
+
 };
 
 //uploadElement(upload, function (err, files) {
@@ -52,16 +74,16 @@ function startSeeding (divId) {
 //})
 
 
-function onFiles (files) {
-    files.forEach(function (file) {
-        console.log("in onfiles"+ file.name +"\n$$$"+ file.size)
-    })
+function onFiles(files) {
+    files.forEach(function(file) {
+        console.log("in onfiles" + file.name + "\n$$$" + file.size);
+    });
 
     // .torrent file = start downloading the torrent
-    files.filter(isTorrentFile).forEach(downloadTorrentFile)
+    files.filter(isTorrentFile).forEach(downloadTorrentFile);
 
     // everything else = seed these files
-    seed(files.filter(isNotTorrentFile))
+    seed(files.filter(isNotTorrentFile));
 }
 
 // dragdrop('body', function (files) {
--- shared/views.py
+++ shared/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-# from .forms import SeedForm 
+# from .forms import SeedForm
 from .models import SharedList
-from django.core.urlresolvers import reverse
+# from django.core.urlresolvers import reverse
 import time
-from django.contrib import messages
+# from django.contrib import messages
+
 
 def seed(request):
-    return render(request,'seed.html')
+    return render(request, 'seed.html')
+
 
 def download(request):
-    return render(request,'downloadmagnet.html',{'torrentURI': SharedList.objects.all()})
+    return render(request, 'downloadmagnet.html', {'torrentURI':
+                                                   SharedList.objects.all()})
+
 
 def postdata(request):
     print(request)
@@ -17,6 +21,29 @@ def postdata(request):
         magnet_link = request.POST['magnetURI']
         original_seeder = request.POST['name']
         start_time = int(time.time())
-        rec = SharedList(magnet_link = magnet_link, original_seeder = original_seeder, start_time= start_time)
+        rec = SharedList(magnet_link=magnet_link,
+                         original_seeder=original_seeder,
+                         start_time=start_time)
         rec.save()
-        return render(request,'downloadmagnet.html')
+        return render(request, 'downloadmagnet.html')
+
+
+def updatedata(request):
+    if request.method == 'POST':
+        # original_seeder = request.POST['name']
+        torrentList = request.POST.getlist('torrent[]')
+        print("Printing getlist")
+        username = None
+        if request.user.is_authenticated():
+            username = request.user.username
+        print(username," printed username")
+        # for torrent in torrentList:
+        #     try:
+        #         print(" in try block")
+        #         magnet_link=SharedList.objects.get(magnet_link=torrent);
+        #         print(magnet_link.seeders)
+        #     except SharedList.DoesNotExist:
+        #         magnet_link=SharedList(magnet_link=torrent,add_time=time(),seeders=[username])
+        #         print(magnet_link," exception  printed torrent")
+        # print(" printedlads.fads")
+        return render(request, 'seed.html')
