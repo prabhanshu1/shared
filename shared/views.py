22dcc448e82f19ed7bc842c2511d45b15b62b526
Aditya <agaditya@cse.iitk.ac.in>
2016-11-12 23:49:50 +0530

    Started making front-end

--- shared/views.py
+++ shared/views.py
@@ -13,6 +13,12 @@ def seed(request):
         username = request.user.username
     return render(request, 'seed.html', {'name': username})
 
+def display(request):
+    username = None
+    if request.user.is_authenticated():
+        username = request.user.username
+    return render(request, 'display.html', {'name': username})
+
 
 def download(request):
     username = None
